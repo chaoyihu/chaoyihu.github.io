0be0325145a70c6e33105729620bfb11c500fa8c
Chaoyi Hu <chaoyihu.work@gmail.com>
2024-09-17 23:32:10 -0700

    runner refined

--- src/components/Animation/Runner.tsx
+++ src/components/Animation/Runner.tsx
@@ -3,49 +3,31 @@ import React from "react";
 const Runner: React.FC<{speed: number}> = ({ speed }) => {
     return (
         <div>
-            <div className="runner-container">
-                {/* The character */}
-                <div className="runner-images">
-                    <img src="/images/animation/cog-5.webp"></img>
-                    <img src="/images/animation/cog-4.webp"></img>
-                    <img src="/images/animation/cog-3.webp"></img>
-                    <img src="/images/animation/cog-2.webp"></img>
-                </div>
-                <div className="runner-images">  {/* duplicate it*/}
-                    <img src="/images/animation/cog-5.webp"></img>
-                    <img src="/images/animation/cog-4.webp"></img>
-                    <img src="/images/animation/cog-3.webp"></img>
-                    <img src="/images/animation/cog-2.webp"></img>
+            <div className='runner-container' style={{zIndex: -10, bottom: 0}}>
+                <div className="runner-immobile">
+                    <img src="/images/animation/runner/runner-robot.gif"></img>
                 </div>
             </div>
-            <div className="runner-container">
+            <div className="runner-container" style={{zIndex: -20}}>
                 {/* The floor */}
-                <div className="runner-images">
-                    <img src="/images/animation/plant-5.webp"></img>
-                    <img src="/images/animation/plant-4.webp"></img>
-                    <img src="/images/animation/plant-3.webp"></img>
-                    <img src="/images/animation/plant-2.webp"></img>
+                <div className="runner-slider">
+                    <img src="/images/animation/runner/runner-floor.webp"></img>
+                    <img src="/images/animation/runner/runner-floor.webp"></img>
                 </div>
-                <div className="runner-images">  {/* duplicate it*/}
-                    <img src="/images/animation/plant-5.webp"></img>
-                    <img src="/images/animation/plant-4.webp"></img>
-                    <img src="/images/animation/plant-3.webp"></img>
-                    <img src="/images/animation/plant-2.webp"></img>
+                <div className="runner-slider">  {/* duplicate it*/}
+                    <img src="/images/animation/runner/runner-floor.webp"></img>
+                    <img src="/images/animation/runner/runner-floor.webp"></img>
                 </div>
             </div>
-            <div className="runner-container">
+            <div className="runner-container" style={{zIndex: -30}}>
                 {/* The landscape */}
-                <div className="runner-images">
-                    <img src="/images/animation/star-5.webp"></img>
-                    <img src="/images/animation/star-4.webp"></img>
-                    <img src="/images/animation/star-3.webp"></img>
-                    <img src="/images/animation/star-2.webp"></img>
+                <div className="runner-slider">
+                    <img src="/images/animation/runner/runner-landscape-1.webp"></img>
+                    <img src="/images/animation/runner/runner-landscape-2.webp"></img>
                 </div>
-                <div className="runner-images">  {/* duplicate it*/}
-                    <img src="/images/animation/star-5.webp"></img>
-                    <img src="/images/animation/star-4.webp"></img>
-                    <img src="/images/animation/star-3.webp"></img>
-                    <img src="/images/animation/star-2.webp"></img>
+                <div className="runner-slider">  {/* duplicate it*/}
+                    <img src="/images/animation/runner/runner-landscape-1.webp"></img>
+                    <img src="/images/animation/runner/runner-landscape-2.webp"></img>
                 </div>
             </div>
         </div>
